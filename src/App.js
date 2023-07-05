a74ebe0619f87b205c1b8dddb1cebf3d3ee6808c
Tobias Ariel Suez <sueztobiasariel@gmail.com>
2023-07-05 09:08:12 -0300

    funciona

--- src/App.js
+++ src/App.js
@@ -1,37 +1,65 @@
-import './App.css';
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from "react";
 
-function App() {
-  const [randomContent, setRandomContent] = useState({});
+const FlagsGame = () => {
+  const [countries, setCountries] = useState([]);
+  const [selectedCountry, setSelectedCountry] = useState(null);
+  const [score, setScore] = useState(0);
+  const [userInput, setUserInput] = useState("");
 
   useEffect(() => {
     fetch("https://countriesnow.space/api/v0.1/countries/flag/images")
       .then((response) => response.json())
-      .then((data) =>{
-        const randomIndex = Math.floor(Math.random() * data.data.length); // Generate a random index
-        const randomContent = data.data[randomIndex];
-        setRandomContent(randomContent);
-      } )
-      .catch((error) => console.error(error));
+      .then((data) => {
+        setCountries(data.data
+          );
+      })
+      .catch((error) => {
+        console.log("Error fetching countries:", error);
+      });
   }, []);
+
+  const handleRandomCountry = () => {
+    const randomIndex = Math.floor(Math.random() * countries.length);
+    const randomCountry = countries[randomIndex];
+    setSelectedCountry(randomCountry);
+    console.log(randomCountry.name);
+  };
+
+  const handleGuess = () => {
+    if (userInput === selectedCountry.name) {
+      setScore(score + 10);
+      handleRandomCountry();
+      setUserInput("");
+    } else {
+      setScore(score - 1);
+      setUserInput("");
+    }
+  };
+
+  useEffect(() => {
+    if (countries.length > 0 && selectedCountry === null) {
+      handleRandomCountry();
+    }
+  }, [countries, selectedCountry]);
 
   return (
     <div>
-        <img id='foto' src={randomContent.flag}></img>
-          {console.log(randomContent)}
-          <form>
-            <label>Que pais es?</label>
-            <input type={"text"}id="username"></input>
-          </form>
-          <button onclick= {saveUsername()} >Save username</button>
-
+      <h1>Flags Handling Game</h1>
+      {selectedCountry && (
+        <div>
+          <img src={selectedCountry.flag} alt={"nombre Bandera"} />
+          <h2>Guess the Country:</h2>
+          <input
+            type="text"
+            value={userInput}
+            onChange={(e) => setUserInput(e.target.value)}
+          />
+          <button onClick={handleGuess}>Guess</button>
+          <p>Score: {score}</p>
+        </div>
+      )}
     </div>
-
   );
-  function saveUsername() {
-    const username = document.getElementById("username").value;
-    localStorage.setItem("username", username);
-  }  
-}
+};
 
-export default App;
+export default FlagsGame;
